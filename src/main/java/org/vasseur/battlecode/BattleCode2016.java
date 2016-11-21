6dea772d6bd2541cac11984e1926d8d5e053e060
Emmanuel Vasseur <evasseur.ext@orange.com>
2016-11-21 18:00:36 +0100

    Move battlecode example implementation

---
+++ src/main/java/org/vasseur/battlecode/BattleCode2016.java
@@ -0,0 +1,11 @@
+package org.vasseur.battlecode;
+
+import org.vasseur.battlecode.IBattleCode2016;
+
+public class BattleCode2016 implements IBattleCode2016 {
+
+    public int sum(int x, int y) {
+        throw new RuntimeException("not implemented yet");
+    }
+
+}
